1a7e246451bbd2e2a28ec47569e020c3efe17424
Marty Friedel <1491079+martyf@users.noreply.github.com>
2019-07-23 17:48:52 +1000

    Intiial commit of ItemHelper.php
    Signed-off-by: Marty Friedel <1491079+martyf@users.noreply.github.com>

---
+++ ItemHelper.php
@@ -0,0 +1,248 @@
+<?php
+
+defined('_JEXEC') or die;
+
+/**
+ * Class ItemHelper
+ *
+ * A helper class designed to perform some common functions to make life easier when working within template overrides
+ * for Joomla 3.7 and above.
+ *
+ * This is designed to work with Joomla 3. We cannot guarantee that this code will run with Joomla 4, currently in
+ * alpha, as it will be introducing new functionality (and deprecating others) that may require this helper be tweaked.
+ * Stay tuned for updates regarding Joomla 4 support.
+ *
+ * This includes specifically helping to streamline access to custom fields, available from Joomla 3.7.
+ *
+ * To use, first, include the class in your code, such as:
+ *  $app  = JFactory::getApplication();
+ *  $path = JPATH_THEMES . DIRECTORY_SEPARATOR . $app->getTemplate() . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'ItemHelper.php';
+ *  JLoader::register('ItemHelper', $path);
+ *
+ * Then, pass an item to the process function:
+ *  ItemHelper::process($this->item)
+ *
+ * Depending on where your override is and how you access the item, it may be $this->item or $item - check your
+ * override usage for more details.
+ *
+ * @author  Marty Friedel
+ * @version 1.0
+ * @see     https://github.com/martyf/joomla-item-helper
+ */
+class ItemHelper
+{
+    /**
+     * Perform processing on a given Item.
+     *
+     * Processing currently involves translating the jcfields array to a jcfieldsnames array for name-based access.
+     *
+     * Additional helper functions have been defined in this class to assist with accessing fields by name (and
+     * returning their group, label, options or value).
+     *
+     * @param stdClass $item The item to process (i.e. an Article)
+     *
+     * @return stdClass The processed input item
+     */
+    public static function process(&$item)
+    {
+        // define a default type for the Item - an article
+        $type = 'com_content.article';
+
+        // if a type alias has been defined, let's update our local type
+        if (isset($item->type_alias))
+        {
+            $type = $item->type_alias;
+        }
+
+        // convert keys to array for easier access
+        $item->jcfieldsnames = [];
+        if (!isset($item->jcfields))
+        {
+            // get the fields if they're not loaded
+            $item->jcfields = FieldsHelper::getFields($type, $item, true);
+        }
+
+        if (is_array($item->jcfields) && count($item->jcfields) > 0)
+        {
+            // loop through the jcfields array
+            foreach ($item->jcfields as $field)
+            {
+                // add a new
+                $item->jcfieldsnames[ $field->name ] = [
+                    // save the name of the field
+                    'name'    => $field->name,
+
+                    // determine the value
+                    // if its an array, and only one, just return the plain value - this is so we don't need to dig
+                    // inside arrays at the template level for single-option values.
+                    'value'   => is_array($field->rawvalue) && count($field->rawvalue) == 1 ? $field->rawvalue[0] : $field->rawvalue,
+
+                    // the display label of the Field
+                    'label'   => $field->label,
+
+                    // The Field Group ID
+                    // This is really useful if you want to filter on iteration
+                    'group'   => $field->group_id,
+
+                    // Field options
+                    // For types like Checkbox or List, this will be all available (possible) options
+                    'options' => $field->fieldparams->get('options')
+                ];
+
+                if ($field->type == 'checkboxes')
+                {
+                    // create an array for our selected items
+                    $selected = [];
+
+                    // loop through all of the options
+                    foreach ($field->fieldparams->get('options') as $option)
+                    {
+                        // check if the selected value(s) has the current option
+                        // if rawvalue is an array, there is 2 or more options - look inside the rawvalue as an array
+                        // if rawvalue is a string, there's only one option - comparitor is as a string
+                        if ((is_array($field->rawvalue) && in_array($option->value, $field->rawvalue)) || $field->rawvalue == $option->value)
+                        {
+                            $selected[] = array(
+                                'name'  => $option->name,
+                                'value' => $option->value
+                            );
+                        }
+                    }
+
+                    // update the value with the selected items
+                    $item->jcfieldsnames[ $field->name ]['value'] = $selected;
+                }
+                else if (ItemHelper::isJSON($item->jcfieldsnames[ $field->name ]['value']))
+                {
+                    // the value is a JSON string - so decode it to be an associative array
+                    $item->jcfieldsnames[ $field->name ]['value'] = json_decode($item->jcfieldsnames[ $field->name ]['value'], true);
+                    //
+                    // Side note:
+                    //
+                    // Repeatable fields that use "editor" will have their HTML stripped out. Best to avoid as the
+                    // Joomla dev team have advised they won't make other changes to repeatable "editor" bugs given
+                    // the Joomla 4 changes to this entire component.
+                    // See https://issues.joomla.org/tracker/joomla-cms/24343
+                    //
+                }
+
+            }
+        }
+
+        // finally, set a flag on the item saying that the process function has been called
+        $item->isItemHelperProcessed = true;
+
+        // return the item, in case you do set it back to the source
+        return $item;
+    }
+
+
+    /**
+     * Get a specific property for a Custom Field within an Item (i.e. Article).
+     *
+     * The Field is the name of the Custom Field, and Property is one one:
+     *  - group
+     *  - label
+     *  - options
+     *  - value
+     *
+     * @param stdClass $item     The item containing the field
+     * @param string   $field    The field name
+     * @param string   $property The property of the field
+     *
+     * @return bool|mixed   Return the property, or false if not found
+     */
+    protected static function getFieldProperty($item, $field, $property)
+    {
+        // check if we have loaded the item already
+        if (!isset($item->isItemHelperProcessed) || $item->isItemHelperProcessed !== true)
+        {
+            ItemHelper::process($item);
+        }
+
+        // check we have jcfieldsnames
+        if (!isset($item->jcfieldsnames))
+        {
+            return false;
+        }
+
+        // look in the jcfieldsnames array for the given field
+        if (array_key_exists($field, $item->jcfieldsnames))
+        {
+            // we found the field, let's return the property
+            return $item->jcfieldsnames[ $field ][ $property ];
+        }
+
+        // make it this far, return false
+        return false;
+    }
+
+    /**
+     * Get a Custom Field's Group ID
+     *
+     * @param stdClass $item  The item with the Custom Field
+     * @param string   $field The field name to find
+     *
+     * @return bool|string   Return the Group, or false if not found
+     */
+    public static function getFieldGroupId($item, $field)
+    {
+        return ItemHelper::getFieldProperty($item, $field, 'group');
+    }
+
+    /**
+     * Get a Custom Field's Label
+     *
+     * @param stdClass $item  The item with the Custom Field
+     * @param string   $field The field name to find
+     *
+     * @return bool|string   Return the Label, or false if not found
+     */
+    public static function getFieldLabel($item, $field)
+    {
+        return ItemHelper::getFieldProperty($item, $field, 'label');
+    }
+
+    /**
+     * Get a Custom Field's Options
+     *
+     * @param stdClass $item  The item with the Custom Field
+     * @param string   $field The field name to find
+     *
+     * @return bool|array   Return the Options, or false if not found
+     */
+    public static function getFieldOptions($item, $field)
+    {
+        return ItemHelper::getFieldProperty($item, $field, 'options');
+    }
+
+    /**
+     * Get a Custom Field's Value
+     *
+     * @param stdClass $item  The item with the Custom Field
+     * @param string   $field The field name to find
+     *
+     * @return bool|string|array   Return the Value, or false if not found
+     *                             Value may be a string or an array
+     */
+    public static function getFieldValue($item, $field)
+    {
+        return ItemHelper::getFieldProperty($item, $field, 'value');
+    }
+
+    /**
+     * Simple helper function to determine if the string is JSON or not
+     *
+     * Defined as public in case we need to use it at the template override level
+     *
+     * @param string $string A string to test
+     *
+     * @return bool True if string is JSON, false otherwise
+     */
+    public static function isJSON($string)
+    {
+        json_decode($string);
+
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# joomla-item-helper
+# Joomla ItemHelper
+
+This Joomla ItemHelper is used in template overrides to make some common actions much easier (and help centralise 
+some repetitive logic so that we don't have to copy and paste certain code over and over again).
+
+This is aimed at developers who create their own templates for Joomla, and are comfortable working with PHP.
+
+## How to use ItemHelper
+
+Within the override PHP file, you need to include the ItemHelper. I put it in my template's folder, under a folder called
+`helpers`. You can place it where you like, but just remember to update the path below.
+
+```PHP
+$app  = JFactory::getApplication();
+$path = JPATH_THEMES . DIRECTORY_SEPARATOR . $app->getTemplate() . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'ItemHelper.php';
+JLoader::register('ItemHelper', $path);
+```
+
+To run the magic, then simply call ItemHelper's `process` method, passing in your item - such as `$this->item` or `$item`, 
+depending on the override type you're working with.
+```PHP
+ItemHelper::process($this->item)
+```
+
+This will update your item, and not override any
+
+## Custom Fields
+
+Custom Fields have been in Joomla 3 since 3.7, and are incredibly powerful and flexible. 
+
+But accessing the Custom Fields at the override level is a little repetitive - and if we need it in multiple overrides,
+that can be a lot of code replication.
+
+Centralising this processing of Custom Fields makes it easier to include in multiple overrides, but keep the core
+processing logic in one place.
+
+### Processing Custom Fields
+
+The ItemHelper's `process` method will look at your item's Custom Fields, and create a new array called `jcfieldsnames`.
+
+### Accessing a Custom Field
+
+After the processing, you can iterate over `$this->item->jcfieldsnames` like any other array.
+
+Alternatively, some helper functions have been created that make it easy for you to get a Custom Field's:
+- label
+- group ID
+- options (for Field Types that have options)
+- value
+
+These helpers will return the property, or false if not found.
+
+To get a specific field's value, use `getFieldValue`. This can return a `string` for single selections, or `array` for 
+multiple (such as checkboxes).
+
+```PHP
+ItemHelper::getFieldValue($this->item, 'my-field');
+```
+
+To get a specific field's label, use `getFieldLabel`.
+
+```PHP
+ItemHelper::getFieldLabel($this->item, 'my-field');
+```
+
+To get a specific field's Field Group ID, use `getFieldGroupId`.
+
+```PHP
+ItemHelper::getFieldGroupId($this->item, 'my-field');
+```
+
+To get a specific field's options, use `getFieldOptions`. This only applies to Field Types that use options, such as List
+or Checkboxes. Other Field Types will return an empty `array`.
+
+```PHP
+ItemHelper::getFieldOptions($this->item, 'my-field');
+```
+
+## Requests or issues
+
+If you're having any issues working with the ItemHelper, 
+[start a new issue](https://github.com/martyf/joomla-item-helper/issues) in this repository.
+
+Any requests for specific field type handling can be added there too.
+
+Need a Joomla expert? I work for [Mity Digital](https://www.mity.com.au), a Melbourne-based web design and development 
+agencey. We love working with Joomla - come and say hello!
